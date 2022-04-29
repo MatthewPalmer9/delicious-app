a99185510e2aa7cff0df888eee33de68fc137e14
Matthew Palmer <matthewpalmer9@outlook.com>
2022-04-29 14:34:47 -0500

    Moved reusable styled components into utils/Common.js & established imports in Popular & Veggie components.

--- src/components/Veggie.js
+++ src/components/Veggie.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import { Wrapper, Card, Gradient } from "../utils/Common";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 export default function Veggie() {
-  return (
-    <div>Veggie</div>
-  )
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+        getPopular();
+    }, []);
+
+    const getPopular = async () => {
+        const check = localStorage.getItem('popular');
+
+        if(check) {
+            setPopular(JSON.parse(check));
+        } else {
+            const api = await fetch(
+                `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+            );
+            const data = await api.json();
+            
+            localStorage.setItem('popular', JSON.stringify(data.recipes));
+            setPopular(data.recipes);
+            console.log(data.recipes);
+        }
+    };
+
+    return (
+        <div>
+            <Wrapper>
+                <h3>Popular Picks</h3>
+                <Splide 
+                    options={{ 
+                        perPage: 4, 
+                        arrows: false, 
+                        pagination: false,
+                        drag: 'free',
+                        gap: '5rem',
+                    }}
+                >
+                    {popular?.map((recipe) => {
+                        return (
+                            <SplideSlide key={recipe.id}>
+                                <Card>
+                                    <p>{recipe.title}</p>
+                                    <img
+                                        src={recipe.image}
+                                        alt={recipe.title}
+                                    />
+                                    <Gradient />
+                                </Card>
+                            </SplideSlide>
+                        );
+                    })}
+                </Splide>
+            </Wrapper>
+        </div>
+    );
 }
